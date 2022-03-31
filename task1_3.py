bdc2de05bff7d8535952006c7dfae8cac12a76c5
Trevor Nyathi <trevorfln7@gmail.com>
2022-03-31 20:02:24 +0200

    add task1_3.py

---
+++ task1_3.py
@@ -0,0 +1,8 @@
+def sixty5_in_nums(number1, number2):
+    sum_numbers = number1 + number2
+    if number1 == 65 or number2 == 65:
+        return True
+    elif sum_numbers == 65:
+        return True
+    else:
+        return False 
